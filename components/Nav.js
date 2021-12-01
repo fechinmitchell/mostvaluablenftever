76b47677ee3117066aca6f84b9c0f6b8d69f0d8b
fechinmitchell <32688003+fechinmitchell@users.noreply.github.com>
2021-12-01 15:49:23 +0100

    Fourth Commit

---
+++ components/Nav.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Link from 'next/link';
+
+export default function Nav() {
+    return (
+        <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
+            <div className="text-xl"><img
+                  src={"/img/logo/1NOFF_Logo.png"}
+                  alt=""/></div>
+            <div className="flex">
+            <nav className="border-b p-1">
+            
+             
+            
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-green-200">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-green-200">
+              Create INOFF
+            </a>
+          </Link>
+          <Link href="/collection">
+            <a className="mr-6 text-green-200">
+              Collection
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-green-200">
+              My INOFFs
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-green-200">
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+            </div>
+        </div>
+    )
+}
---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0xC010C93165d49ae257eA52634181510c847B0221"
+  export const nftaddress = "0xcC23ab110F9206a62972eAC0e955CEF160C916DB"
+   
