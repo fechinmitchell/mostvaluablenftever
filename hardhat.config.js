0916ecf93a8328fe7f4586ea863c36cdc8165e8c
Fechinmitchell <fechinmitchell1996@gmail.com>
2022-04-04 21:17:28 -0700

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
 const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+// const projectId = "646007f81b6e46b9a746194ee77ac1e8"
  
 module.exports = {
   defaultNetwork: "hardhat",
@@ -12,12 +13,16 @@ module.exports = {
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      // url: `https://polygon-mumbai.infura.io/v3/${projectId}`
+
       url: "https://rpc-mumbai.maticvigil.com/",
       accounts: [privateKey]
     },
     matic: {
       // Infura
       // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      // url: `https://polygon-mainnet.infura.io/v3/${projectId}`
+
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
