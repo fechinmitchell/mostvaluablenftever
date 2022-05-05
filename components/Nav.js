d56d78b12dbdcfee36c22699574aca93eceda8ee
Fechinmitchell <fechinmitchell1996@gmail.com>
2022-05-04 17:38:58 -0700

    new front page video

--- components/Nav.js
+++ components/Nav.js
@@ -14,27 +14,27 @@ export default function Nav() {
             
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-6 text-yellow-500">
+            <a className="mr-6 text-yellow-300">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-yellow-500">
+            <a className="mr-6 text-yellow-300">
               Create INOFF
             </a>
           </Link>
           <Link href="/collection">
-            <a className="mr-6 text-yellow-200">
+            <a className="mr-6 text-yellow-300">
               Collection
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-green-500">
+            <a className="mr-6 text-yellow-300">
               The One and Only
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-yellow-500">
+            <a className="mr-6 text-yellow-300">
               Creator Dashboard
             </a>
           </Link>
