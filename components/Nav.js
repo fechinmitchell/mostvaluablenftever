ba22f7b153c2b5a5fbca03b63f0be093a0ca8de5
Fechinmitchell <fechinmitchell1996@gmail.com>
2022-05-04 17:30:07 -0700

    ..

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Nav() {
     return (
-        <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
+        <div className="py-4 px-8 bg-black text-white flex justify-between">
             <div className="text-xl"><img
                   src={"/img/logo/MVNFTE_Logo.png"}
                   alt=""/></div>
@@ -14,27 +14,27 @@ export default function Nav() {
             
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-6 text-green-200">
+            <a className="mr-6 text-yellow-500">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-green-200">
+            <a className="mr-6 text-yellow-500">
               Create INOFF
             </a>
           </Link>
           <Link href="/collection">
-            <a className="mr-6 text-green-200">
+            <a className="mr-6 text-yellow-200">
               Collection
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-green-200">
+            <a className="mr-6 text-green-500">
               The One and Only
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-green-200">
+            <a className="mr-6 text-yellow-500">
               Creator Dashboard
             </a>
           </Link>
