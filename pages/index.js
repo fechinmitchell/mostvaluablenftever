80873876cbac2d3e0437ce05a030c22e28a036e0
Fechinmitchell <fechinmitchell1996@gmail.com>
2022-05-04 18:25:10 -0700

    NEW UNDER LOGO

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,16 @@ export default function Home() {
             <div className= "font-bold mt-4 bg-white-300 text-white rounded p-4 shadow-lg">
               <a href="#">
                 <img
-                  src={ "/img/logo/1NOFF_Logo.png"}
+                  src={ "/img/logo/MVNFTE_Logo_UNDER.png"}
                   alt=""
                 />
               </a>
             </div>
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden"></p>
 
-        <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">Our first collection "The Artless Collection" will be available soon. This collection will be ultra rare on a basis of one of one items.</p>
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">Our first collection "The Artless Collection" will be available soon. This collection will be ultra rare on a basis of one of one items.</p>
 
-        <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">How it works: When you purchase this NFT we will reach out to you to verify that you are the true owner. You will then tell us which size you would like your piece of clothing in and we will send you it. This is all verified on the matic blockchain and a QR code on the piece of clothing to verify the authenticity of the piece of clothing</p>
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">How it works: When you purchase this NFT we will reach out to you to verify that you are the true owner. You will then tell us which size you would like your piece of clothing in and we will send you it. This is all verified on the matic blockchain and a QR code on the piece of clothing to verify the authenticity of the piece of clothing</p>
       
     </div>
   )
