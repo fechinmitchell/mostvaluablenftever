cc4d147879aee8920a6fcbd9f377a32608898724
Fechinmitchell <fechinmitchell1996@gmail.com>
2022-05-05 10:47:38 -0700

    .

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div>
       <div className="text-xl">
-                  <video width="1600" height="500px" loop autoPlay muted>
+                  <video width="1600" height="300px" loop autoPlay muted>
                   <source src="/Video/FrontPage.mp4" type="video/mp4"/>
                   </video>
                   </div>
                   
-            <div className= "font-bold mt-4 bg-black text-white rounded p-4 shadow-lg">
+            <div className= "font-bold mt-4 bg-white text-white rounded p-4 shadow-lg">
               <a href="#">
                 <img
                   src={ "/img/logo/MVNFTE_Logo_UNDER.png"}
@@ -21,9 +21,23 @@ export default function Home() {
             </div>
             <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden"></p>
 
-            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">Our first collection "The Artless Collection" will be available soon. This collection will be ultra rare on a basis of one of one items.</p>
+            <h2 className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">The Utility of this NFT is as follows</h2>
 
-            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">How it works: When you purchase this NFT we will reach out to you to verify that you are the true owner. You will then tell us which size you would like your piece of clothing in and we will send you it. This is all verified on the matic blockchain and a QR code on the piece of clothing to verify the authenticity of the piece of clothing</p>
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">You'll be announced King of the NFT World</p>
+
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">You will get a unique NFT(A line code assocaited with a picture host on IPFS in the Etheruem Network which you can flex to everyone)</p>
+
+            <p className="w-5/6 m-auto mb-5 text-center borderRadius='100px' shadow rounded-xl overflow-hidden">You'll have a piece of work from the famous </p>
+
+            <div class="card">
+              <img src="img_avatar.png" alt="Avatar" style="width:100%"/>
+            <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect and Engineer</p>
+          </div>
+          
+</div>
+
       
     </div>
   )
